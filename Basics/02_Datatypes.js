40017a6b50addb8391b02217408f8698813f6750
RegMa-exe <Vidhishekhawat12345@gmail.com>
2024-07-07 09:28:29 +0000

    Datatype

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer Version(all new synatc modulues would get used)
+
+/*alert("hello") */  //we are using node js not browser so alert notifaction cant be displayed
+
+let name="Vidhan"//String datatype      
+let age = 18//number datatype
+let isloggedin = false//boolean datatype
+
+/*  Types of Datatype */
+//number(from o to 2*53)
+//bigint(for big numbers)
+//string=> "xyz"
+//boolean=> True/false
+//null(insted of showing 0 we show null)
+//undefined
+ //object
+
+ console.log(typeof undefined);//Undefined 
+ console.log(typeof null);//object
