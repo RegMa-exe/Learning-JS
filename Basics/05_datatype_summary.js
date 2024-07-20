1c099b42c716e04c4a5c3be97274c9b2987c4b1c
Vidhan Shekhawat <Vidhishekhawat12345@gmail.com>
2024-07-20 07:22:06 +0000

    05

---
+++ Basics/05_datatype_summary.js
@@ -0,0 +1,39 @@
+// Primitive Data Types//
+
+//Total 7 Catogeries :String,Number,Null,boolean,undefined,Symbol,BigInt(for scintifc value or big no)
+
+let Name="Vidhan"
+console.log(Name);
+
+let Score=18
+console.log(Score);
+
+let TempOutisde=null
+console.log(TempOutisde);
+
+let Isloggedin=true
+console.log(Isloggedin);
+
+
+/*SYMBOL*/  
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Symbol
+
+const id=Symbol('@')
+console.log(id);
+
+
+//Refrence Type (Non Primitive)
+
+//Arrays,Objects,,Functions
+
+//Array=> Isntead of giving each variable of each diffrent element we use array to give it it one and then extract it accc to our need 
+
+const Heros=[ "Hulk" , "Ironman", "Vision"]
+let Hero2=Heros[2]//Array start from 0
+console.log(Hero2);
+
+
+
+
+
